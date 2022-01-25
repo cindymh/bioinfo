715a569efcc9bfc9b900236786f131773fca1dc1
cindymh <cindymhidajat@gmail.com>
2022-01-25 12:09:26 +1100

    Create pattern_count.py

---
+++ pattern_count.py
@@ -0,0 +1,7 @@
+# This function returns counts of the occurence of k-mer pattern in a text
+def pattern_count(text, pattern):
+    count = 0
+    for i in range(len(text)-len(pattern)+1):
+        if text[i:i+len(pattern)] == pattern:
+            count = count+1
+    return count 
